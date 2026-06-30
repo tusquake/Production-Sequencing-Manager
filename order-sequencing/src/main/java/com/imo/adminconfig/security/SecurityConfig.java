c5f1aa8c2290cfd29a865b8623130114a675880e
Tushar Seth <tusharrealme@gmail.com>
2026-06-30 11:56:37 +0530

    Fix: Permit static resources publicly in production SecurityConfig

--- order-sequencing/src/main/java/com/imo/adminconfig/security/SecurityConfig.java
+++ order-sequencing/src/main/java/com/imo/adminconfig/security/SecurityConfig.java
@@ -31,6 +31,15 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                    "/",
+                    "/index.html",
+                    "/assets/**",
+                    "/favicon.svg",
+                    "/icons.svg",
+                    "/*.js",
+                    "/*.css",
+                    "/*.png",
+                    "/*.ico",
                     "/swagger-ui.html",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
